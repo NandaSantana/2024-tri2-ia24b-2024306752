bea8a8c5ece687f75fed2bf2725760bb5289196f
NandaSantana <nandasbbs9@gmail.com>
2024-09-10 11:19:07 -0300

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,12 @@
-const btstooglenav = document.querySelectorAll ('button.tooglenav')
 
+//Uma variavel que procura uma lista dentro do document de botoes com a classe tooglenav
+const btstooglenav = document.querySelectorAll ('button.tooglenav')
 const nv= document.querySelector ('nav')
 
 btstooglenav.forEach (functon(bt){
+    //adiciona uma ação para o evento, ou seja, quando clicar outra coisa vai acontecer. ele vai pegar a lista de class no nav, 
+    //e se ele tiver a class open ele tira a class e se não tiver ele adiciona.    
+    
     bt.addEventListener('click',function(){
         nv.classList.toogle('open')
     })
